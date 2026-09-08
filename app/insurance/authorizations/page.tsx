43a0ddd0e681fa1ef56dab53c4e3ad3afc96d481
Sanjeev K <sanjukesu13@gmail.com>
2026-09-08 12:45:39 +0530

    Add insurance authorization pipeline

---
+++ app/insurance/authorizations/page.tsx
@@ -0,0 +1,2 @@
+import RolePipelinePage from "@/components/RolePipelinePage";
+export default function InsuranceAuthorizations() { return <RolePipelinePage role="insurance_agent" section="authorizations" />; }
